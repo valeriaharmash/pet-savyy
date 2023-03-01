5c199321de6f68df47235d14d59aa0c07d73fc7d
Chris Ennis <ce2433@columbia.edu>
2023-03-01 17:36:46 -0500

    linked cart to server data

--- client/Routes.js
+++ client/Routes.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import { AuthForm } from './components/AuthForm';
-import Home from './components/Home';
-import { getUserByToken } from './store';
-import { getUserToken, isLoggedIn } from './utils';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { AuthForm } from "./components/AuthForm";
+import Home from "./components/Home";
+import { getUserByToken } from "./store";
+import { getUserToken, isLoggedIn } from "./utils";
+import Cart from "./features/cart";
 
 const Router = ({}) => {
   const dispatch = useDispatch();
@@ -21,12 +22,14 @@ const Router = ({}) => {
       {user ? (
         <Routes>
           <Route path="/home" element={<Home />} />
+          <Route path="/user/:userId/cart" element={<Cart />} />
           <Route path="*" element={<Home />} />
         </Routes>
       ) : (
         <Routes>
           <Route exact path="/login" element={<AuthForm mode="login" />} />
           <Route exact path="/signup" element={<AuthForm mode="signup" />} />
+          <Route path="/user/:userId/cart" element={<Cart />} />
           <Route path="*" element={<AuthForm mode="login" />} />
         </Routes>
       )}
--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { createLogger } from 'redux-logger';
-import auth from './slices/auth';
+import { configureStore } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
+import auth from "./slices/auth";
+import cartReducer from "../features/cartSlice";
 
 const store = configureStore({
   reducer: {
     auth: auth,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(createLogger()),
 });
 
 export default store;
-export * from './slices/auth';
+export * from "./slices/auth";
