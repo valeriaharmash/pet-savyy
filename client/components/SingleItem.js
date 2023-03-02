f3f9e3f411be23d7be8563d014b2fa938ff31a42
Stephen Parker <sparkerr.96@gmail.com>
2023-03-02 14:17:08 -0800

    add add to cart functionality to all page and single page

--- client/components/SingleItem.js
+++ client/components/SingleItem.js
@@ -1,18 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { fetchSingleItem } from '../store/slices/items';
+import { fetchSingleItem, addItemToCart  } from '../store/slices/items';
 
 const SingleItem = () => {
   const dispatch = useDispatch();
   const { itemId } = useParams();
   const item = useSelector((state) => state.items.selectedItem);
+  const user = useSelector((state) => state.auth.user);
+  let userId = null;
+  if (user) {
+    userId = user.id;
+  }
 
   useEffect(() => {
     if (itemId) {
       dispatch(fetchSingleItem(itemId));
     }
   }, [itemId]);
+
+  const handleAddToCart = async (itemId) => {
+    await dispatch(addItemToCart({ userId, itemId }));
+  };
 
   if (!item.id) return null;
 
@@ -41,7 +50,7 @@ const SingleItem = () => {
             : 'Out of Stock'}
         </div>
         <p>Price: {`${item.price}$`}</p>
-        <button>Add to cart</button>
+        <button onClick={() => handleAddToCart(item.id)}>Add to cart</button>  {/*just need to be able to pass a qty as a second argument in handleAddToCart to fix only adding one to cart*/}
       </div>
     </div>
   );
