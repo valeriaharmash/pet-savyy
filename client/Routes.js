537b7baaa5d4a24eb2f9bbb1d0ca2b75ab9f5454
Lisa Kraisriwatana <humanwave11@gmail.com>
2023-03-03 16:55:19 -0500

    user routes snd frontend validation

--- client/Routes.js
+++ client/Routes.js
@@ -1,42 +1,50 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
-import { AuthForm, Home, SingleItem, UpdateItem, Main } from "./components";
-import { getUserByToken } from "./store";
-import { isLoggedIn } from "./utils";
-import Cart from "./features/cart";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import {
+  AuthForm,
+  Home,
+  SingleItem,
+  UpdateItem,
+  Main,
+  AllUsers,
+} from './components';
+import { getUserByToken } from './store';
+import { isLoggedIn } from './utils';
+import Cart from './features/cart';
 
 const Router = ({}) => {
-	const dispatch = useDispatch();
-	const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
 
-	useEffect(() => {
-		if (isLoggedIn()) {
-			dispatch(getUserByToken());
-		}
-	}, []);
+  useEffect(() => {
+    if (isLoggedIn()) {
+      dispatch(getUserByToken());
+    }
+  }, []);
 
-	if (user && user.role === "admin") {
-		return (
-			<Routes>
-				<Route path="/home" element={<Home />} />
-				<Route exact path="/items/:itemId" element={<SingleItem />} />
-				<Route exact path="/items/:itemId/update" element={<UpdateItem />} />
-				<Route path="*" element={<Main />} />
-			</Routes>
-		);
-	} else {
-		return (
-			<Routes>
-				<Route exact path="/login" element={<AuthForm mode="login" />} />
-				<Route exact path="/signup" element={<AuthForm mode="signup" />} />
-				<Route path="/home" element={<Home />} />
-				<Route exact path="/items/:itemId" element={<SingleItem />} />
-				<Route path="/user/:userId/cart" element={<Cart />} />
-				<Route path="*" element={<Main />} />
-			</Routes>
-		);
-	}
+  if (user && user.role === 'admin') {
+    return (
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route exact path="/items/:itemId" element={<SingleItem />} />
+        <Route exact path="/items/:itemId/update" element={<UpdateItem />} />
+        <Route exact path="/users" element={<AllUsers />} />
+        <Route path="*" element={<Main />} />
+      </Routes>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route exact path="/login" element={<AuthForm mode="login" />} />
+        <Route exact path="/signup" element={<AuthForm mode="signup" />} />
+        <Route path="/home" element={<Home />} />
+        <Route exact path="/items/:itemId" element={<SingleItem />} />
+        <Route path="/user/:userId/cart" element={<Cart />} />
+        <Route path="*" element={<Main />} />
+      </Routes>
+    );
+  }
 };
 
 export default Router;
