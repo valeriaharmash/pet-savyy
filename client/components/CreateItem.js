57b1d065f8e7087970e4ec9281129d80cc579a8f
Valeriia Harmash <valeriaharmash@gmail.com>
2023-03-03 13:29:50 -0500

    complete createItem component

--- client/components/CreateItem.js
+++ client/components/CreateItem.js
@@ -1,4 +1,67 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
 import { createItem } from "../store/slices/items";
+
+const CreateItem = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [itemName, setItemName] = useState("");
+	const [itemDescription, setItemDescription] = useState("");
+	const [itemPrice, setItemPrice] = useState("");
+	const [itemStock, setItemStock] = useState("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await dispatch(createItem(itemName, itemDescription, itemPrice, itemStock));
+		setItemName("");
+		setItemDescription("");
+		setItemPrice("");
+		setItemStock("");
+		navigate("/items");
+	};
+
+	return (
+		<div className="row apart">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="itemName">Item Name:</label>
+				<input
+					name="itemName"
+					value={itemName}
+					onChange={(e) => setItemName(e.target.value)}
+				/>
+
+				<label htmlFor="itemDescription">Item Description:</label>
+				<input
+					name="itemDescription"
+					value={itemDescription}
+					onChange={(e) => setItemDescription(e.target.value)}
+				/>
+
+				<label htmlFor="itemPrice">Item Price:</label>
+				<input
+					name="itemPrice"
+					value={itemPrice}
+					onChange={(e) => setItemPrice(e.target.value)}
+				/>
+
+				<label htmlFor="itemStock">Number of items in stock:</label>
+				<input
+					name="itemStock"
+					value={itemStock}
+					onChange={(e) => setItemStock(e.target.value)}
+				/>
+
+				<button
+					disabled={!itemName && !itemDescription && !itemPrice && !itemStock}
+					type="submit"
+				>
+					Submit
+				</button>
+				<Link to="/items">Cancel</Link>
+			</form>
+		</div>
+	);
+};
+
+export default CreateItem;
--- client/components/index.js
+++ client/components/index.js
@@ -1,8 +1,9 @@
-import { AuthForm } from './AuthForm';
-import { Home } from './Home';
-import Items from './Items';
-import Navbar from './Navbar';
-import SingleItem from './SingleItem';
-import UpdateItem from './UpdateItem';
+import { AuthForm } from "./AuthForm";
+import { Home } from "./Home";
+import Items from "./Items";
+import Navbar from "./Navbar";
+import SingleItem from "./SingleItem";
+import UpdateItem from "./UpdateItem";
+import CreateItem from "./CreateItem";
 
-export { AuthForm, Home, Items, Navbar, SingleItem, UpdateItem };
+export { AuthForm, Home, Items, Navbar, SingleItem, UpdateItem, CreateItem };
