78e0a239dc43fbf782f93dfa3312ef73e2f88af0
Lisa Kraisriwatana <humanwave11@gmail.com>
2023-03-03 17:10:20 -0500

    more updates, user routes, frontend validation

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import { selectAllUsers, fetchUsers } from '../store/slices/users';
 // import { useDispatch, useSelector } from 'react-redux';
 
@@ -8,7 +8,7 @@ const AllUsers = () => {
     <div>
       <h1>All Users</h1>
       <ul>
-        <li>In progress</li>
+        <li>In Progress</li>
       </ul>
     </div>
   );
