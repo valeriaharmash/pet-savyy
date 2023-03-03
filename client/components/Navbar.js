8aa247d55963cdb43b5fbe8b39b37ff013f3176d
Valeriia Harmash <valeriaharmash@gmail.com>
2023-03-03 14:11:39 -0500

    rearranged navbar and routes depending on the user role

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,50 +1,49 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { setUser } from '../store';
-import { removeUserToken } from '../utils';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { setUser } from "../store";
+import { removeUserToken } from "../utils";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.auth.user);
-  return (
-    <div>
-      <h1>Grace Shopper Pet Store</h1>
-      <nav>
-        {user ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <Link to="/home">Costumes</Link>
-            <Link to="/home">Supplies</Link>
-            <Link to="/home">My Account</Link>
-            <Link to={`/user/${user.id}/cart`}>Cart</Link>
-            <a
-              onClick={() => {
-                dispatch(setUser(null));
-                removeUserToken();
-                navigate('/');
-              }}
-            >
-              Logout
-            </a>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/home">Home</Link>
-            <Link to="/home">Costumes</Link>
-            <Link to="/home">Supplies</Link>
-            <Link to="/cart">Cart</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        )}
-      </nav>
-      <hr />
-    </div>
-  );
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const user = useSelector((state) => state.auth.user);
+	return (
+		<div>
+			<h1>Grace Shopper Pet Store</h1>
+			<nav>
+				<Link to="/home">Home</Link>
+				<Link to="/home">Costumes</Link>
+				<Link to="/home">Supplies</Link>
+				{/* We have to get rid of user.id here in order to support :guest" experience. */}
+				{user && user.role !== "admin" && (
+					<Link to={`/user/${user.id}/cart`}>Cart</Link>
+				)}
+				{user ? (
+					<div>
+						{/* The navbar will show these links after you log in */}
+						<Link to="/home">My Account</Link>
+						<a
+							onClick={() => {
+								dispatch(setUser(null));
+								removeUserToken();
+								navigate("/");
+							}}
+						>
+							Logout
+						</a>
+					</div>
+				) : (
+					<div>
+						{/* The navbar will show these links before you log in */}
+						<Link to="/login">Login</Link>
+						<Link to="/signup">Sign Up</Link>
+					</div>
+				)}
+			</nav>
+			<hr />
+		</div>
+	);
 };
 
 export default Navbar;
