20819d83fca71a5bc24ce5153188425d8fb83db5
Valeriia Harmash <valeriaharmash@gmail.com>
2023-03-03 19:26:20 -0500

    working basic items functionality

--- client/Routes.js
+++ client/Routes.js
@@ -1,18 +1,11 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
-import {
-	AuthForm,
-	Home,
-	SingleItem,
-	UpdateItem,
-	Main,
-	AllUsers,
-	CreateItem,
-} from "./components";
-import { getUserByToken } from "./store";
-import { isLoggedIn } from "./utils";
-import Cart from "./features/cart";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { AllUsers, AuthForm, CreateItem, Home, Main, SingleItem, UpdateItem, } from './components';
+import { getUserByToken } from './store';
+import { isLoggedIn } from './utils';
+import Cart from './features/cart';
+import { Checkout } from './features';
 
 const Router = ({}) => {
   const dispatch = useDispatch();
@@ -24,29 +17,30 @@ const Router = ({}) => {
     }
   }, []);
 
-	if (user && user.role === "admin") {
-		return (
-			<Routes>
-				<Route path="/home" element={<Home />} />
-				<Route exact path="/items/:itemId" element={<SingleItem />} />
-				<Route exact path="/items/:itemId/update" element={<UpdateItem />} />
-				<Route exact path="/users" element={<AllUsers />} />
-				<Route path="*" element={<Main />} />
-			</Routes>
-		);
-	} else {
-		return (
-			<Routes>
-				<Route exact path="/login" element={<AuthForm mode="login" />} />
-				<Route exact path="/signup" element={<AuthForm mode="signup" />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/checkout" element={<Checkout />} />
-				<Route exact path="/items/:itemId" element={<SingleItem />} />
-				<Route path="/user/:userId/cart" element={<Cart />} />
-				<Route path="*" element={<Main />} />
-			</Routes>
-		);
-	}
+  if (user && user.role === 'admin') {
+    return (
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route exact path="/items/add" element={<CreateItem/>}/>
+        <Route exact path="/items/:itemId" element={<SingleItem/>}/>
+        <Route exact path="/items/:itemId/update" element={<UpdateItem/>}/>
+        <Route exact path="/users" element={<AllUsers/>}/>
+        <Route path="*" element={<Main/>}/>
+      </Routes>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route exact path="/login" element={<AuthForm mode="login"/>}/>
+        <Route exact path="/signup" element={<AuthForm mode="signup"/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+        <Route exact path="/items/:itemId" element={<SingleItem/>}/>
+        <Route path="/user/:userId/cart" element={<Cart/>}/>
+        <Route path="*" element={<Main/>}/>
+      </Routes>
+    );
+  }
 };
 
 export default Router;
--- client/components/CreateItem.js
+++ client/components/CreateItem.js
@@ -1,67 +1,77 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { createItem } from "../store/slices/items";
 
 const CreateItem = () => {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-	const [itemName, setItemName] = useState("");
-	const [itemDescription, setItemDescription] = useState("");
-	const [itemPrice, setItemPrice] = useState("");
-	const [itemStock, setItemStock] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [stock, setStock] = useState('');
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		await dispatch(createItem(itemName, itemDescription, itemPrice, itemStock));
-		setItemName("");
-		setItemDescription("");
-		setItemPrice("");
-		setItemStock("");
-		navigate("/items");
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await dispatch(createItem({name, description, price, stock}));
+    setName('');
+    setDescription('');
+    setPrice('');
+    setStock('');
+    navigate('/');
+  };
 
-	return (
-		<div className="row apart">
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="itemName">Item Name:</label>
-				<input
-					name="itemName"
-					value={itemName}
-					onChange={(e) => setItemName(e.target.value)}
-				/>
+  return (
+    <div className="column">
+      <form onSubmit={handleSubmit}>
+        <div className="row apart">
+          <label htmlFor="itemName">Item Name:</label>
+          <input
+            name="itemName"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-				<label htmlFor="itemDescription">Item Description:</label>
-				<input
-					name="itemDescription"
-					value={itemDescription}
-					onChange={(e) => setItemDescription(e.target.value)}
-				/>
+        <div className="row apart">
+          <label htmlFor="itemDescription">Item Description:</label>
+          <input
+            name="itemDescription"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
 
-				<label htmlFor="itemPrice">Item Price:</label>
-				<input
-					name="itemPrice"
-					value={itemPrice}
-					onChange={(e) => setItemPrice(e.target.value)}
-				/>
+        <div className="row apart">
+          <label htmlFor="itemPrice">Item Price:</label>
+          <input
+            name="itemPrice"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
 
-				<label htmlFor="itemStock">Number of items in stock:</label>
-				<input
-					name="itemStock"
-					value={itemStock}
-					onChange={(e) => setItemStock(e.target.value)}
-				/>
+        <div className="row apart">
+          <label htmlFor="itemStock">Number of items in stock:</label>
+          <input
+            name="itemStock"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+          />
+        </div>
 
-				<button
-					disabled={!itemName && !itemDescription && !itemPrice && !itemStock}
-					type="submit"
-				>
-					Submit
-				</button>
-				<Link to="/items">Cancel</Link>
-			</form>
-		</div>
-	);
+        <div className="row around">
+          <button
+            disabled={!name && !description && !price && !stock}
+            type="submit"
+          >
+            Submit
+          </button>
+          <button onClick={() => navigate('/items')}>Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateItem;
