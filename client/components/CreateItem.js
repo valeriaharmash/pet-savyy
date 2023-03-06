bd06bd3785b0c9203bbce03dbf7ddc2b038f67c6
Valeriia Harmash <valeriaharmash@gmail.com>
2023-03-06 18:57:50 -0500

    lint

--- client/components/CreateItem.js
+++ client/components/CreateItem.js
@@ -4,85 +4,85 @@ import { useNavigate } from 'react-router-dom';
 import { createItem } from '../store/slices/items';
 
 const CreateItem = () => {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-	const [name, setName] = useState('');
-	const [description, setDescription] = useState('');
-	const [price, setPrice] = useState('');
-	const [stock, setStock] = useState('');
-	const [imageUrl, setImageUrl] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [stock, setStock] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		await dispatch(createItem({ name, description, price, stock, imageUrl }));
-		setName('');
-		setDescription('');
-		setPrice('');
-		setStock('');
-		setImageUrl('');
-		navigate('/');
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await dispatch(createItem({ name, description, price, stock, imageUrl }));
+    setName('');
+    setDescription('');
+    setPrice('');
+    setStock('');
+    setImageUrl('');
+    navigate('/');
+  };
 
-	return (
-		<div className="column">
-			<form onSubmit={handleSubmit}>
-				<div className="row apart">
-					<label htmlFor="itemName">Item Name:</label>
-					<input
-						name="itemName"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-					/>
-				</div>
+  return (
+    <div className="column">
+      <form onSubmit={handleSubmit}>
+        <div className="row apart">
+          <label htmlFor="itemName">Item Name:</label>
+          <input
+            name="itemName"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-				<div className="row apart">
-					<label htmlFor="itemDescription">Item Description:</label>
-					<input
-						name="itemDescription"
-						value={description}
-						onChange={(e) => setDescription(e.target.value)}
-					/>
-				</div>
+        <div className="row apart">
+          <label htmlFor="itemDescription">Item Description:</label>
+          <input
+            name="itemDescription"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
 
-				<div className="row apart">
-					<label htmlFor="itemPrice">Item Price:</label>
-					<input
-						name="itemPrice"
-						value={price}
-						onChange={(e) => setPrice(e.target.value)}
-					/>
-				</div>
+        <div className="row apart">
+          <label htmlFor="itemPrice">Item Price:</label>
+          <input
+            name="itemPrice"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
 
-				<div className="row apart">
-					<label htmlFor="itemStock">Number of items in stock:</label>
-					<input
-						name="itemStock"
-						value={stock}
-						onChange={(e) => setStock(e.target.value)}
-					/>
-				</div>
+        <div className="row apart">
+          <label htmlFor="itemStock">Number of items in stock:</label>
+          <input
+            name="itemStock"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+          />
+        </div>
 
-				<div className="row apart">
-					<label htmlFor="imageUrl">Image:</label>
-					<input
-						name="itemUrl"
-						value={imageUrl}
-						onChange={(e) => setImageUrl(e.target.value)}
-					/>
-				</div>
+        <div className="row apart">
+          <label htmlFor="imageUrl">Image:</label>
+          <input
+            name="itemUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+        </div>
 
-				<div className="row around">
-					<button
-						disabled={!name && !description && !price && !stock}
-						type="submit"
-					>
-						Submit
-					</button>
-					<button onClick={() => navigate('/items')}>Cancel</button>
-				</div>
-			</form>
-		</div>
-	);
+        <div className="row around">
+          <button
+            disabled={!name && !description && !price && !stock}
+            type="submit"
+          >
+            Submit
+          </button>
+          <button onClick={() => navigate('/items')}>Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateItem;
--- client/features/checkout.js
+++ client/features/checkout.js
@@ -3,102 +3,102 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Checkout = () => {
-	const location = useLocation();
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
-	const [email, setEmail] = useState('');
-	const [address, setAddress] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
 
-	const user = useSelector((state) => state.auth.user);
-	const orderId = useSelector((state) => state.cart.orderId);
+  const user = useSelector((state) => state.auth.user);
+  const orderId = useSelector((state) => state.cart.orderId);
 
-	useEffect(() => {
-		if (user) {
-			setFirstName(user.firstName);
-			setLastName(user.lastName);
-			setEmail(user.email);
-			setAddress(user.address);
-		}
-	}, [user]);
+  useEffect(() => {
+    if (user) {
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setEmail(user.email);
+      setAddress(user.address);
+    }
+  }, [user]);
 
-	return (
-		<div className="column">
-			<h2>Checkout</h2>
-			<form>
-				<div className="row apart">
-					<label htmlFor="firstName" className="form-label">
-						First Name:
-					</label>
-					<input
-						className="form"
-						name="firstName"
-						value={firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
-				</div>
-				<div className="row apart">
-					<label htmlFor="lastName" className="form-label">
-						Last Name:
-					</label>
-					<input
-						className="form"
-						name="lastName"
-						value={lastName}
-						onChange={(e) => setLastName(e.target.value)}
-					/>
-				</div>
-				<div className="row apart">
-					<label htmlFor="email" className="form-label">
-						Email:
-					</label>
-					<input
-						className="form"
-						name="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</div>
-				<div className="row apart">
-					<label htmlFor="address" className="form-label">
-						Shipping Address:
-					</label>
-					<input
-						className="form"
-						name="address"
-						value={address}
-						onChange={(e) => setAddress(e.target.value)}
-					/>
-				</div>
-				<div className="row around">
-					<button
-						disabled={!firstName || !lastName || !email || !address}
-						onClick={() =>
-							navigate('/checkout/pay', {
-								state: {
-									userId: user && user.id,
-									amount: Number(location.state.total) * 100,
-									orderId,
-									shippingAddress: address,
-								},
-							})
-						}
-					>
-						Continue to Payment
-					</button>
-					<button onClick={() => navigate('/')}>Cancel</button>
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div className="column">
+      <h2>Checkout</h2>
+      <form>
+        <div className="row apart">
+          <label htmlFor="firstName" className="form-label">
+            First Name:
+          </label>
+          <input
+            className="form"
+            name="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className="row apart">
+          <label htmlFor="lastName" className="form-label">
+            Last Name:
+          </label>
+          <input
+            className="form"
+            name="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className="row apart">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            className="form"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="row apart">
+          <label htmlFor="address" className="form-label">
+            Shipping Address:
+          </label>
+          <input
+            className="form"
+            name="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <div className="row around">
+          <button
+            disabled={!firstName || !lastName || !email || !address}
+            onClick={() =>
+              navigate('/checkout/pay', {
+                state: {
+                  userId: user && user.id,
+                  amount: Number(location.state.total) * 100,
+                  orderId,
+                  shippingAddress: address,
+                },
+              })
+            }
+          >
+            Continue to Payment
+          </button>
+          <button onClick={() => navigate('/')}>Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Checkout;
 
 {
-	/* <button
+  /* <button
           onClick={handleSave}
           disabled={!editAllowed}
           className="form-label"
