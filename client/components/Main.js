79def0dbed85f4c962062137eaf675f9e8cf5916
Valeriia Harmash <valeriaharmash@gmail.com>
2023-04-17 18:10:22 -0400

    removed sales banner

--- client/components/Main.js
+++ client/components/Main.js
@@ -4,29 +4,26 @@ import { Link } from 'react-router-dom';
 import { avocadoPic, saleBanner } from './Images';
 
 const Main = () => {
-  return (
-    <div className='main'>
-      <div className='item-list'>
-        <Link to='/signup'>
-          {' '}
-          <img src={saleBanner}></img>
-        </Link>
-        <LimitedItems maxItems={12} />
-      </div>
-      <div className='welcome-page'>
-        <div className='welcome'>
-          <p>
-            {' '}
-            <img className='welcome-image' src={avocadoPic} />
-          </p>
-          <p>
-            Hi, I'm Palta! Welcome to Grace Shopper Pet Store, a one-stop shop
-            for all your pet's needs.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="main">
+			<div className="item-list">
+				<Link to="/signup"> </Link>
+				<LimitedItems maxItems={12} />
+			</div>
+			<div className="welcome-page">
+				<div className="welcome">
+					<p>
+						{' '}
+						<img className="welcome-image" src={avocadoPic} />
+					</p>
+					<p>
+						Hi, I'm Palta! Welcome to Grace Shopper Pet Store, a one-stop shop
+						for all your pet's needs.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Main;
