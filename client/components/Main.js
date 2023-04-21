b5be2ca3ba5a4d9a748ae7f710b9100576dd4ec0
Valeriia Harmash <valeriaharmash@gmail.com>
2023-04-21 15:49:14 -0400

    refactored main page

--- client/components/Main.js
+++ client/components/Main.js
@@ -8,18 +8,6 @@ const Main = () => {
 			<div className="item-list">
 				<Link to="/signup"> </Link>
 			</div>
-			<div className="welcome-page">
-				<div className="welcome">
-					<p>
-						{' '}
-						<img className="welcome-image" src={avocadoPic} />
-					</p>
-					<p>
-						Hi, I'm Palta! Welcome to Grace Shopper Pet Store, a one-stop shop
-						for all your pet's needs.
-					</p>
-				</div>
-			</div>
 		</div>
 	);
 };
